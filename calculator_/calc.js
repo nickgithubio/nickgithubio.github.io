e16f464d2afa171794b6e54017f3de790c42e001
Nikhil Chaudhary <148001022+nickgithubio@users.noreply.github.com>
2023-11-19 17:25:14 +0530

    Add files via upload

---
+++ calculator_/calc.js
@@ -0,0 +1,20 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function deleteLast() {
+    var displayValue = document.getElementById('display').value;
+    document.getElementById('display').value = displayValue.slice(0, -1);
+}
